6dc455cdf46a7d4357062197dca2664f66b32169
Владимир <vovakalini4ev@gmail.com>
2019-04-11 16:37:20 +0300

    Добавьте файлы проекта.

---
+++ Task_04/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Task_04
+{
+
+    /// Задача - дорабатываем будильник
+    /// необходимо написать метод, который позволит считать, сколько времени осталось до того, как зазвонит будильник
+
+
+    class Program
+    {
+
+       public static void Main(string[] args)
+        {
+
+
+            var wakeUp = DateTime.Now.AddMinutes(5);
+            foreach (DateTime value in AlarmClockTimer(wakeUp))
+            {
+                
+            }
+        }
+              /*Работает, но лучше погуглю как реализовать без костылей */     
+        
+        public static  IEnumerable<DateTime> AlarmClockTimer( DateTime wakeUp)
+        {
+           
+           for (; ; )
+            {
+                Console.WriteLine((wakeUp - DateTime.Now).ToString(@"dd\.hh\:mm\:ss"));
+                Thread.Sleep(1000);
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
